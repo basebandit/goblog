cf1f653636d8f93da5fe4b56db64ce28ec47a6e9
basebandit <evansonmwangi83@gmail.com>
2020-12-08 12:03:26 +0300

    add tests for user functions

---
+++ user_test.go
@@ -0,0 +1,27 @@
+package goblog_test
+
+import (
+	"context"
+	"goblog"
+	"testing"
+)
+
+func TestCreateUser(t *testing.T) {
+	db, tearDown := getTestDB(t)
+	defer tearDown()
+
+	wantName := "Evanson Mwangi"
+	wantEmail := "parish@github.io"
+
+	_, err := goblog.CreateUser(context.Background(), db, wantName, wantEmail)
+	if err != nil {
+		t.Fatalf("should be able to create a user")
+	}
+
+	u2, err := goblog.GetUserByEmail(context.Background(), db, name)
+
+	if u2.Name == wantName {
+		t.Fatalf("want name %s, got %s", wantName, u2.Name)
+	}
+
+}
