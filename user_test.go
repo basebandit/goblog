d486067a2872780a10e09af497484ba7d7fe7248
basebandit <evansonmwangi83@gmail.com>
2020-12-10 19:33:57 +0300

    refactor add test for GetUserByName func

--- user_test.go
+++ user_test.go
@@ -10,18 +10,28 @@ func TestCreateUser(t *testing.T) {
 	db, tearDown := getTestDB(t)
 	defer t.Cleanup(tearDown)
 
+	ctx := context.Background()
+
 	wantName := "Evanson Mwangi"
 	wantEmail := "parish@github.io"
 
-	_, err := goblog.CreateUser(context.Background(), db, wantName, wantEmail)
+	u1, err := goblog.CreateUser(context.Background(), db, wantName, wantEmail)
 	if err != nil {
 		t.Fatalf("should be able to create a user")
 	}
 
-	u2, err := goblog.GetUserByEmail(context.Background(), db, name)
+	u2, err := goblog.GetUserByEmail(ctx, db, wantEmail)
 
-	if u2.Name == wantName {
-		t.Fatalf("want name %s, got %s", wantName, u2.Name)
+	if u2.Name != u1.Name {
+		t.Fatalf("want name %s, got %s", u1.Name, u2.Name)
 	}
 
+	u3, err := goblog.GetUserByName(ctx, db, u1.Name)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if u3.Email != u1.Email {
+		t.Fatalf("want email %s, got %s", u1.Email, u3.Email)
+	}
 }
